2d39c7109c90e661ed6d2357f0fdc733f1a10ea3
dkswjdrbs123 <dkswjdrbs12@naver.com>
2022-06-20 17:08:04 +0900

    yml파일에서 request,response header 등록 방법

--- src/main/java/com/msa/study/spring_cloud_gateway/config/FillterConfig.java
+++ src/main/java/com/msa/study/spring_cloud_gateway/config/FillterConfig.java
@@ -5,11 +5,11 @@ import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-@Configuration
+//@Configuration
 public class FillterConfig {
 
     // yml에서 설정하였던 부분을 java로 설정
-    @Bean
+//    @Bean
     public RouteLocator getewayRoutes(RouteLocatorBuilder builder) {
         return builder.routes()
                 .route(r -> r.path("/first-service/**")
