e1e7c6822a095560ef8b52133d8e70f6cc2f1c9c
dkswjdrbs123 <dkswjdrbs12@naver.com>
2022-06-20 16:58:40 +0900

    preperties.yml을 java코드로 설정해보기

---
+++ src/main/java/com/msa/study/spring_cloud_gateway/config/FillterConfig.java
@@ -0,0 +1,25 @@
+package com.msa.study.spring_cloud_gateway.config;
+
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class FillterConfig {
+
+    // yml에서 설정하였던 부분을 java로 설정
+    @Bean
+    public RouteLocator getewayRoutes(RouteLocatorBuilder builder) {
+        return builder.routes()
+                .route(r -> r.path("/first-service/**")
+                        .filters(f -> f.addRequestHeader("first-request","first-request-header")
+                                        .addResponseHeader("first-response","first-response-header"))
+                        .uri("http://localhost:8081"))
+                .route(r -> r.path("/second-service/**")
+                        .filters(f -> f.addRequestHeader("second-request","second-request-header")
+                                .addResponseHeader("second-response","second-response-header"))
+                        .uri("http://localhost:8081"))
+                .build();
+    }
+}
